8845bdbbb75a9bb153f8cc54797659bd9180e5e8
serkus01 <7erkus@gmail.com>
2020-11-28 17:34:02 +0900

    add  dash2

---
+++ dash.py
@@ -0,0 +1,68 @@
+#-*-coding: UTF-8 -*-
+#!/usr/bin/env python
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Coke', 'value': 'COKE'},
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Apple', 'value': 'AAPL'}
+        ],
+        value='COKE'
+    ),
+    dcc.Graph(id='my-graph')
+], style={'width': '500'})
+
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from pandas_datareader import data as web
+from datetime import datetime as dt
+
+app = dash.Dash('Hello World')
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Coke', 'value': 'COKE'},
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Apple', 'value': 'AAPL'}
+        ],
+        value='COKE'
+    ),
+    dcc.Graph(id='my-graph')
+], style={'width': '500'})
+
+@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
+def update_graph(selected_dropdown_value):
+    df = web.DataReader(
+        selected_dropdown_value,
+        'google',
+        dt(2017, 1, 1),
+        dt.now()
+    )
+    return {
+        'data': [{
+            'x': df.index,
+            'y': df.Close
+        }],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+#app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+
+if __name__ == '__main__':
+    app.run_server()
